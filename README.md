edf3d46ed590f5d9f6db4deaba14a0792e91b3b8
fcomitani <federico.comitani@gmail.com>
2017-05-13 19:23:28 +0100

    Added clustering functionality

--- README.md
+++ README.md
@@ -9,7 +9,13 @@ of the network with selected features.
 If run as a standalone program, simpsom.py will run a toy model, where a number of colors will be mapped from the 3D
 RGB space to the 2D network map and clustered according to their similarity in the origin space.
 
+## Dependencies
 
+- Numpy 1.11.0 (older versions may work)
+- Matplotlib 1.5.1 (older versions may work)
+
+- Sklearn 0.15 (older versions may work), optional, needed only for clustering with algorithms other 
+	than Quality Treshold.
 
 ## Example of usage:
 
@@ -24,14 +30,17 @@ net.train(10000, 0.01)
 net.save('colorExample_weights')
 	
 #Print a map of the network nodes and colour them according to the first feature of the dataset
-	and then according to the distance between each node and its neighbours.
+#and then according to the distance between each node and its neighbours.
 net.nodes_graph(colnum=0)
 net.diff_graph()
 	
 #Project the datapoints on the new 2D network map.
-net.proj_graph(raw_data, labels=labels)
+net.project(raw_data, labels=labels)
+
+#Cluster the datapoints according to the Mean Shift algorithm from sklearn.
+net.cluster(raw_data, type='MeanShift')
 ```
 
 ## TO DOs:
 
-- Implement a way to extract the obtained clusters as indexes.
+- Update the available cluster algorithms from sklearn
--- simpsom.py
+++ simpsom.py
@@ -1,6 +1,6 @@
 #########################################################################
 ################ SimpSOM (Simple Self Organizing Maps) ##################
-############################### v1.0.0 ##################################
+############################### v1.1.0 ##################################
 ######################### F. Comitani @2017 #############################
 #########################################################################
 
@@ -230,9 +230,11 @@ class somNet:
 			plt.show()
 		plt.clf()
 
-	def proj_graph(self, array, colnum=0, labels=[], show=False, printout=True):
+	def project(self, array, colnum=0, labels=[], show=False, printout=True):
 
-		"""Plot a 2D map with as implemented in nodes_graph and adds circles to the bmu
+		"""Project the datapoints of a given array to the 2D space of the 
+			SOM by calculating the bmus. If requested plot a 2D map with as 
+			implemented in nodes_graph and adds circles to the bmu
 			of each datapoint in a given array.
 
 		Args:
@@ -241,11 +243,10 @@ class somNet:
 			show (bool, optional): Choose to display the plot.
 			printout (bool, optional): Choose to save the plot to a file.
 			
+		Returns:
+			(list): bmu x,y position for each input array datapoint. 
+			
 		"""
-
-		""" Call nodes_graph to first build the 2D map of the nodes. """
-		
-		self.nodes_graph(colnum, False, False)
 
 		bmuList,cls=[],[]
 		for i in range(array.shape[0]):
@@ -255,24 +256,150 @@ class somNet:
 			else: 
 				cls.append(array[i,colnum])
 
-		#be careful, x,y are inverted in nodes_graph imshow()		
-		if self.colorEx==True:
-			printName='colorProjection.png'
-			plt.scatter([pos[1] for pos in bmuList],[pos[0] for pos in bmuList], color=cls, edgecolor='black')
+		if show==True or printout==True:
+		
+			""" Call nodes_graph to first build the 2D map of the nodes. """
+		
+			self.nodes_graph(colnum, False, False)
+
+			#be careful, x,y are inverted in nodes_graph imshow()		
+			if self.colorEx==True:
+				printName='colorProjection.png'
+				plt.scatter([pos[1] for pos in bmuList],[pos[0] for pos in bmuList], color=cls, edgecolor='black')
+				plt.title('Datapoints Projection')
+			else:
+				printName='projection_'+str(colnum)+'.png'
+				plt.scatter([pos[1] for pos in bmuList],[pos[0] for pos in bmuList], color=cls, edgecolor='black', cmap=cm.viridis)
+				plt.title('Datapoints Projection #' +  str(colnum))
+
+			if labels!=[]:
+				for label, x, y in zip(labels, [pos[1] for pos in bmuList],[pos[0] for pos in bmuList]):
+					plt.annotate(label, xy=(x,y), xytext=(-0.25, 0.25), textcoords='offset points', ha='right', va='bottom') 
+			
+
+			if printout==True:
+				plt.savefig(printName, bbox_inches='tight', dpi=600)
+			if show==True:
+				plt.show()
+			plt.clf()
+		
+		""" Print the x,y coordinates of bmus, useful for the clustering function. """
+		
+		return [[pos[0],pos[1]] for pos in bmuList] 
+		
+		
+	def cluster(self, array, type='qtresh', cutoff=5, quant=0.2, \
+					savefile=True, show=False, printout=True):
+	
+		"""Clusters the data in a given array according to the SOM trained map.
+			The clusters can also be plotted.
+
+		Args:
+			array (np.array): An array containing datapoints to be clustered.
+			type (str, optional): The type of clustering to be applied, so far only quality treshold (qtresh) 
+				algorithm is directly implemented, other algorithms require sklearn.
+			cutoff(float, optional): Cutoff for the quality treshold algorithm. This also doubles as
+				maximum distance of two points to be considered in the same cluster with DBSCAN.
+			quant(float, optional): Quantile used to calculate the bandwidth of the mean shift algorithm.
+			mdist(float, optional): 
+			saveFile (bool, optional): Choose to save the resulting clusters in a text file.
+			show (bool, optional): Choose to display the plot.
+			printout (bool, optional): Choose to save the plot to a file.
+			
+		Returns:
+			(list of int): A nested list containing the clusters with indexes of the input array points.
+			
+		"""
+
+		""" Call project to first find the bmu for each array datapoint, but without producing any graph. """
+
+		bmuList = self.project(array, show=False, printout=False)
+		clusters=[]
+
+		if type=='qtresh':
+			
+			""" Cluster according to the quality treshold algorithm (slow!). """
+	
+			tmpList=range(len(bmuList))
+			while len(tmpList)!=0:
+				qtList=[]
+				#the cluster list for each point contains the point itself
+				#so that it gets automatically
+				#deleted at the next loop without having to worry about it
+				for i in tmpList:
+					qtList.append([])
+					for j in tmpList:
+						if np.sqrt((bmuList[j][0]-bmuList[i][0])*(bmuList[j][0]-bmuList[i][0])\
+							+(bmuList[j][1]-bmuList[i][1])*(bmuList[j][1]-bmuList[i][1])) <= cutoff:
+							qtList[-1].append(j)
+				clusters.append(max(qtList,key=len))
+				for el in clusters[-1]:
+					tmpList.remove(el)
+
 		else:
-			printName='projection_'+str(colnum)+'.png'
-			plt.scatter([pos[1] for pos in bmuList],[pos[0] for pos in bmuList], color=cls, edgecolor='black', cmap=cm.viridis)
+		
+			""" Cluster according to algorithms implemented in sklearn. """
+			#TODO: for now only MeanShift and DBSCAN are implemented, add the rest of the algorithms	
+		
+			try:
+				#TODO: clean all this mess
+				from sklearn import cluster
+		
+				if type=='MeanShift':
+					bandwidth = cluster.estimate_bandwidth(np.asarray(bmuList), quantile=quant, n_samples=500)
+					cl = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True).fit(bmuList)
+				
+				if type=='DBSCAN':
+					cl = cluster.DBSCAN(eps=cutoff, min_samples=5).fit(bmuList)		
+				
+				clLabs = cl.labels_					
+					
+				for i in np.unique(clLabs):
+					clList=[]
+					tmpList=range(len(bmuList))
+					for j,k in zip(tmpList,clLabs):
+						if i==k:
+							clList.append(j)
+					clusters.append(clList)		
+			except:
+				print 'Unexpected error: ', sys.exc_info()[0]
+				raise
+		
+		if savefile==True:
+			file=open(type+'_clusters.txt', 'w')
+			for line in clusters:
+				for id in line: file.write(str(id)+' ')
+				file.write('\n')
+			file.close()
+		
+		if printout==True or show==True:
+			
+			np.random.seed(0)
+			printName=type+'_clusters.png'
+			
+			fig, ax = plt.subplots()
+			
+			for i in range(len(clusters)):
+				randCl = "#%06x" % np.random.randint(0, 0xFFFFFF)
+				xc,yc=[],[]
+				for c in clusters[i]:
+					#again, invert y and x to be consistent with the previous maps
+					xc.append(bmuList[int(c)][1])
+					yc.append(bmuList[int(c)][0])	
+				ax.scatter(xc, yc, color=randCl, label='cluster'+str(i))
 
-		if labels!=[]:
-			for label, x, y in zip(labels, [pos[1] for pos in bmuList],[pos[0] for pos in bmuList]):
-				plt.annotate(label, xy=(x,y), xytext=(-0.25, 0.25), textcoords='offset points', ha='right', va='bottom') 
-		plt.title('Datapoints Projection #' +  str(colnum))
+			plt.gca().invert_yaxis()
+			plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)			
+			ax.set_title('Clusters')
 
-		if printout==True:
-			plt.savefig(printName, bbox_inches='tight', dpi=600)
-		if show==True:
-			plt.show()
-		plt.clf()
+			if printout==True:
+				plt.savefig(printName, bbox_inches='tight', dpi=600)
+			if show==True:
+				plt.show()
+			plt.clf()	
+			
+		return clusters
+
 		
 class somNode:
 
@@ -380,6 +507,6 @@ if __name__ == "__main__":
 	net.save('colorExample_weights')
 	net.nodes_graph()
 	net.diff_graph()
-	net.proj_graph(raw_data, labels=labels)
+	net.project(raw_data, labels=labels)
 	
 	print "done!"
