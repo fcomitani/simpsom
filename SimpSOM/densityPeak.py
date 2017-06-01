a5f99ab6275d3b9cb089391d2a581b0b906ce1fc
fcomitani <federico.comitani@gmail.com>
2017-06-01 19:02:34 +0200

    Working on densityPeak

--- SimpSOM/densityPeak.py
+++ SimpSOM/densityPeak.py
@@ -13,11 +13,13 @@ WARNING: Work in Progress... Not tested!
 
 """
 
+import sys
 import numpy as np
 from operator import attrgetter
 import warnings
+import matplotlib.pyplot as plt
 
-class point:
+class pt:
 	""" Class for the points to cluster. """
 
 	def __init__(self, coordinates):
@@ -38,22 +40,33 @@ class point:
 		self.delta=sys.maxsize
 		self.dists=[]
 
-	def set_rho(self, listPoints, typeFunc='step'):
+	def set_dist(self, coll):
+	
+	 	"""Calculate the distances from all other points in a collection. 
+
+	 	Args:
+			coll (collection): collection containing all the points of the dataset used to calculate the distances. """
+
+	 	for p2 in coll.points:
+	 			if self!=p2: self.dists.append(dist(self,p2))
+
+
+	def set_rho(self, coll, typeFunc='step'):
 
 		"""Calculate the density of the single point for a given dataset. [Deprecated]
 
 		Args:
-			listPoints (list, point): List containing all the points of the dataset used to calculate the density. 
+			coll (collection): collection containing all the points of the dataset used to calculate the density. 
 			typeFunc (str): step function type (step, gaussian kernel or logistic).
 
 		"""
 
 		warnings.warn('Setting individual rhos is deprecated, use the collection.set_rhos() instead!', DeprecationWarning)
 
-		if self not in listPoints:
+		if self not in coll.points:
 			print('WARNING: calculating the density for a point that was not found in the dataset, make sure to be consistent with your data!')
 		
-		for p2 in listPoints:
+		for p2 in coll.points:
 			if self!=p2: 
 				if typeFunc=='step':
 					self.rho=self.rho+step(self,p2,self.refd)
@@ -65,40 +78,41 @@ class point:
 					""" Raise exception if metric other then euclidean is used. """
 					raise NotImplementedError('Only step, gaussian kernel or logistic functions are implemented')
 
-	def set_delta(self, listPoints):
+	def set_delta(self, coll):
 
 		"""Calculate the distance of the point from higher density points. [Deprecated]
 
 		Args:
-			listPoints (list, point): List containing all the points of the dataset used to calculate the distance.
+			coll (collection): collection containing all the points of the dataset used to calculate the distance.
 
 		"""
 		
 		warnings.warn('Setting individual deltas is deprecated, use the collection.set_deltas() instead!', DeprecationWarning)
 		
-		if self not in listPoints:
+		if self not in coll.points:
 			print('WARNING: calculating the distance for a point that was not found in the dataset, make sure to be consistent with your data!')
 
 		mind=sys.maxsize
 		distHigh, distLow= [], []
 		
-		for p2 in listPoints:
+		for p2 in coll.points:
 			if self!=p2:
 				if self.rho<p2.rho:
 					distHigh.append(dist(self,p2))
 				else:
 					distsLow.append(dist(self,p2))
 		
-		if len(distHigh)>0: self.delta=np.min(distHigh)
 		""" If the point has maximal rho, then return max distance """
-		else: return self.delta=np.max(distLow)
+
+		if len(distHigh)>0: self.delta=np.min(distHigh)
+		else: self.delta=np.max(distLow)
 
 
 class collection:
 
 	"""Class for a collection of point objects. """
 
-	def __init__(self, coorArray, typeFunc='step', percent=0.2):
+	def __init__(self, coorArray, typeFunc='gaussian', percent=0.2):
 		
 		"""	Generate a collection of point objects from an array containing their coordinates.
 	
@@ -111,35 +125,38 @@ class collection:
 	
 		self.points=[]
 		for coors in coorArray:
-			self.points.append(point(coors))	
+			self.points.append(pt(coors))	
 
 		#Not sure about this... quintile of distances?
 		##########CHECK#############################
 
-		set_dists(points)
+		self.set_dists()
+		#############fix this#####################
+		index=int(np.round(len(self.points)*percent))
 
-		index=np.round(len(self.points)*percent)
 		alldists=[]
-		for point in self.points:
-			alldists=alldists+point.dists
-		alldists=alldists.sort
-		refd=alldists[index]
+		for p1 in self.points:
+			for p2 in self.points:
+				if p1<p2: alldists.append(dist(p1,p2))
+
+		alldists.sort()
+		self.refd=alldists[index]
 
 		""" Make sure rhos are set before setting deltas """
 
-		set_rhos(points, typeFunc)
-		set_delta(points)	
+		self.set_rhos(typeFunc)
+		self.set_deltas()	
 
 
 	def set_dists(self):
 	
-		"""Calculate the distance matrix for all points. """
+	 	"""Calculate the distance matrix for all points. """
 
-		for p1 in self.points:
-			for p2 in self.points:
-				if p1<p2: 
-					d=dist(p1,p2)
-					p1.dists.append(d), p2.dists.append(d)
+	 	for p1 in self.points:
+	 		for p2 in self.points:
+	 			if p1<p2: 
+	 				d=dist(p1,p2)
+	 				p1.dists.append(d), p2.dists.append(d)
 
 
 	def set_rhos(self, typeFunc='step'):
@@ -155,14 +172,14 @@ class collection:
 			for p2 in self.points:
 				if p1<p2: 
 					if typeFunc=='step':
-						p1.rho=p1.rho+step(p1,p2,refd)
-						p2.rho=p2.rho+step(p1,p2,refd)
+						p1.rho=p1.rho+step(p1,p2,self.refd)
+						p2.rho=p2.rho+step(p1,p2,self.refd)
 					elif typeFunc=='gaussian':
-						p1.rho=p1.rho+gaussian(p1,p2,refd)
-						p2.rho=p2.rho+gaussian(p1,p2,refd)
+						p1.rho=p1.rho+gaussian(p1,p2,self.refd)
+						p2.rho=p2.rho+gaussian(p1,p2,self.refd)
 					elif typeFunc=='logistic':
-						p1.rho=p1.rho+sigmoid(p1,p2,refd)
-						p2.rho=p2.rho+sigmoid(p1,p2,refd)
+						p1.rho=p1.rho+sigmoid(p1,p2,self.refd)
+						p2.rho=p2.rho+sigmoid(p1,p2,self.refd)
 					else:
 						""" Raise exception if metric other then euclidean is used """
 						raise NotImplementedError('Only step, gaussian kernel or logistic functions are implemented')
@@ -175,7 +192,7 @@ class collection:
 		for p1 in self.points:
 			for p2 in self.points:
 			 	# No need to re-set the distances
-				#d=dist(p1,p2)
+				d=dist(p1,p2)
 				#p1.dists.append(d), p2.dists.append(d)
 				if p1<p2: 
 					if p1.rho<p2.rho and d<p1.delta: p1.delta=d
@@ -183,8 +200,9 @@ class collection:
 	
 		""" If the point has maximal rho, then return max distance """
 
-		pmax=np.max(self.allPartners, key=attrgetter('rho'))
-		pmax.rho=np.max(pmax.dists)
+		pmax=max(self.points, key=attrgetter('rho'))
+		pmax.set_dist(self)
+		pmax.rho=max(pmax.dists)
 
 def dist(p1,p2, metric='euclid'):
 
@@ -204,8 +222,8 @@ def dist(p1,p2, metric='euclid'):
 		if len(p1.coor)!=len(p2.coor): raise ValueError('Points must have the same dimensionality!')
 		else:
 			diffs=0
-			for i in range(len(p1)): 
-				diffs=diffs+((p1[i]-p2[i])*(p1[i]-p2[i]))
+			for i in range(len(p1.coor)): 
+				diffs=diffs+((p1.coor[i]-p2.coor[i])*(p1.coor[i]-p2.coor[i]))
 				return np.sqrt(diffs)
 	else:
 		""" Raise exception if metric other then euclidean is used """
@@ -259,11 +277,23 @@ def sigmoid(p1, p2, sigma):
 
 	"""
 
-	return np.exp(-1.0*(1.0+np.exp((dist(p1,p2))/sigma)
+	return np.exp(-1.0*(1.0+np.exp((dist(p1,p2))/sigma)))
 
 
 
 if __name__ == "__main__":
 
-	return 1
+  	print("Testing...")
+	samples1 = np.random.multivariate_normal([0, 0], [[1, 0.1],[0.1, 1]], 100)
+	samples2 = np.random.multivariate_normal([10, 10], [[2, 0.5],[0.5, 2]], 100)
+	samples = np.concatenate((samples1,samples2), axis=0)
+#	plt.plot(samples[:, 0], samples[:, 1], '.')
+#	plt.show()
+
+  	pts=collection(samples)
+  	print samples[0]
+  	print pts.points[0].coor
+  	print pts.points[0].delta
+  	print pts.points[0].rho
+
 
