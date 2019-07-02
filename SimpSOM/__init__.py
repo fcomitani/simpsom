e1d17cd5594187d9f0b5c9921310b0ca46ca3089
fcomitani <federico.comitani@gmail.com>
2019-07-02 11:30:26 -0400

    add extended palette

--- SimpSOM/__init__.py
+++ SimpSOM/__init__.py
@@ -354,7 +354,8 @@ class somNet:
             colname = str(colnum)
 
         if labels != []:
-            colors = ["#ff0000", "#e0ffff", "#a9a9a9", "#ff69b4", "#966fd6"]
+            colors = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c',
+            		  '#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']
             class_assignment = {}
             counter = 0
             for i in range(len(labels)):
